cb6442f98dea1e913d116df46d85b41053f55df6
daryal89 <34196597+daryal89@users.noreply.github.com>
2022-06-15 12:36:41 -0400

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -2,7 +2,7 @@ public class Simple {
 
 public static void main (String args []){
 
-System.out.println("Welcome to clouk knowledge for devops")
+System.out.println("Welcome to United States of America")
 }
 
 }
